a26388dfe8b3b58d49ad162ca66fbffb6eb24f3c
Andrew Liu <liu.andrew.x@gmail.com>
2020-05-24 17:38:27 -0400

    Support reminders (Fixes #1)

--- dtl/bot.py
+++ dtl/bot.py
@@ -1,8 +1,12 @@
 import os
 import logging
+from datetime import timedelta
+from typing import Optional
+import asyncio as aio
 
 from dotenv import load_dotenv
 import discord  # type: ignore
+from humanize import naturaldelta  # type: ignore
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -23,11 +27,47 @@ FRANZ = 356508428358778891
 DARSHAN = 284163385128386561
 
 bot = discord.Client()
+pending_reminder: Optional[aio.Task] = None
 
 
 def this_person_wants_to_play_league(msg: str) -> bool:
-    keywords = ["sl?", "dtl?"]
-    return any(map(lambda x: x in msg.lower(), keywords))
+    keywords = ["sl", "dtl"]
+    return any(map(lambda x: x in msg.lower(), keywords)) and msg[-1] == "?"
+
+
+def parse_timer(msg: str) -> Optional[timedelta]:
+    in_split = msg.split(" in ")
+    if len(in_split) != 2:
+        logger.debug('No substring of "in" detected...')
+        return None
+
+    space_split = in_split[1].split(" ")
+    if len(space_split) != 2:
+        logger.debug("Time and unit wasn't split by a space")
+        return None
+
+    try:
+        value = float(space_split[0])
+    except ValueError as e:
+        logger.debug("Numerical value failed to parse: %s", e)
+        return None
+
+    if value < 1:
+        logger.debug("Duration is not positive: %s", value)
+        return None
+
+    full_unit = space_split[1]
+    unit = full_unit[0].lower()
+    if unit == "h" and value <= 3:
+        return timedelta(hours=value)
+
+    if unit == "m" and value >= 5:
+        return timedelta(minutes=value)
+
+    logger.debug(
+        "Time unit didn't parse (%s) or value was out of bounds (%s)", full_unit, value
+    )
+    return None
 
 
 @bot.event
@@ -41,11 +81,10 @@ async def on_message(message):
     guild = message.guild
     channel = message.channel
     author_id = message.author.id
+    badger_hole = bot.get_channel(TBH_GENERAL_CHANNEL)
+    darshan = bot.get_user(DARSHAN)
 
-    if author_id == bot.user.id:
-        return
-
-    if channel.id not in [TBH_DEBUG_CHANNEL, SL_CHANNEL]:
+    if author_id == bot.user.id or channel.id not in [TBH_DEBUG_CHANNEL, SL_CHANNEL]:
         return
 
     if (
@@ -53,6 +92,11 @@ async def on_message(message):
         and bot.user not in message.mentions
     ):
         return
+
+    # To avoid spamming.
+    if channel.id == TBH_DEBUG_CHANNEL:
+        badger_hole = bot.get_channel(TBH_DEBUG_CHANNEL)
+        darshan = bot.get_user(ANDREW)
 
     logger.info(message)
 
@@ -63,27 +107,71 @@ async def on_message(message):
         else:
             logger.warning("emoji {emoji} was not found", emoji=emoji)
 
+    async def remind_about_league(duration: timedelta) -> None:
+        try:
+            logger.info("Coroutine started!")
+            await aio.sleep(duration.total_seconds())
+            await badger_hole.send(
+                ":alarm_clock: This is a reminder that "
+                f"{message.author.mention} expressed interest in League "
+                f"{naturaldelta(duration)} ago! Are we droppin'?"
+            )
+            global pending_reminder  # pylint: disable=global-statement
+            pending_reminder = None
+            logger.info("Coroutine completed!")
+        except aio.CancelledError:
+            logger.info("Coroutine was cancelled!")
+            await channel.send(
+                "(As FYI, I already had an active reminder. "
+                "The old reminder has been cancelled.)"
+            )
+
+    # Interact with Suite++
     await react_with("feelsgoodman")
     await channel.send(
         f"Hello @here! :wave: "
         f"{message.author.mention} would like to play some League! "
-        f"Are you interested? "
         f"(I've also pinged Darshan in the other server. :100:)"
     )
+
+    timediff = parse_timer(message.content)
+
+    async def maybe_send_reminder_msg(discord_channel, pronoun: str) -> None:
+        if timediff is None:
+            return
+
+        await discord_channel.send(
+            f"My proprietary state-of-the-art AI-powered NLP algorithm has "
+            f"detected that {pronoun} would like to play in "
+            f"{naturaldelta(timediff)}. I'll remind you then! :timer:"
+        )
+
+    await maybe_send_reminder_msg(channel, "you")
+    if timediff is not None:
+        global pending_reminder  # pylint: disable=global-statement
+        if pending_reminder is not None:
+            # Cancel any pending reminders.
+            pending_reminder.cancel()
+        # Create a coroutine to remind the channel.
+        pending_reminder = aio.create_task(remind_about_league(timediff))
+
     await channel.send(
         "To get my attention, :robot: just "
-        "say something in this channel with the substring `SL?` or `DTL?` "
+        "ask a question in this channel with the substring `SL` or `DTL` "
         "or mention me in this channel."
     )
+
     if author_id == ANDREW:
         await channel.send(
             "Oh look, Andrew pinged me. Hi daddy! :heart: :heart: :heart:"
         )
 
-    if channel.id == SL_CHANNEL:
-        badger_hole = bot.get_channel(TBH_GENERAL_CHANNEL)
-        darshan = bot.get_user(DARSHAN)
-        await badger_hole.send(f"{darshan.mention} League?")
+    # Interact with The Badger Hole
+    await badger_hole.send(
+        f"Hello {darshan.mention}! :wave: {message.author.mention} wonders "
+        "if you're interested in some League!"
+    )
+    await maybe_send_reminder_msg(badger_hole, "he")
 
 
 if __name__ == "__main__":
--- tests/test_dtl.py
+++ tests/test_dtl.py
@@ -1,5 +1,49 @@
+from datetime import timedelta
+
 from dtl import __version__
+from dtl.bot import this_person_wants_to_play_league as tpwtpl, parse_timer
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_tpwtpl():
+    # Basic cases
+    assert tpwtpl("SL?")
+    assert tpwtpl("sl?")
+    assert tpwtpl("DTL?")
+    assert tpwtpl("sl?")
+
+    # Cases with other stuff before the question mark
+    assert tpwtpl("SL alsdkfjalskdjfladf?")
+    assert tpwtpl("DTL in 69 minutes?")
+
+    # Negative cases
+    assert not tpwtpl("potatoes?")
+    assert not tpwtpl("So league?")
+    assert not tpwtpl("SL")
+    assert not tpwtpl("DTL")
+
+
+def test_parse_timer():
+    assert parse_timer("SL?") is None
+    assert parse_timer("SL asdf 5 min?") is None
+    assert parse_timer("SL in -1 hour?") is None
+    assert parse_timer("SL 1 hour?") is None
+
+    # Parsing minutes
+    assert parse_timer("SL in 15 minutes?") == timedelta(minutes=15)
+    assert parse_timer("SL in 5 min?") == timedelta(minutes=5)
+    assert parse_timer("SL in 10 m?") == timedelta(minutes=10)
+
+    # Parsing hours
+    assert parse_timer("DTL in 1 hour?") == timedelta(hours=1)
+    assert parse_timer("DTL in 3 hrs?") == timedelta(hours=3)
+    assert parse_timer("DTL in 1.5 h?") == timedelta(hours=1.5)
+
+    # Out-of-bound values
+    assert parse_timer("DTL in 4 hours?") is None
+    assert parse_timer("DTL in 5 hours?") is None
+    assert parse_timer("DTL in 4 minutes?") is None
+    assert parse_timer("DTL in 3 minutes?") is None
